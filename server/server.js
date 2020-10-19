2262818567d56a4500f0fc0a3afdf75591e02ccd
Kim Sejun <sejun3278@gmail.com>
2020-10-19 14:25:23 +0900

    'heroku-test-2'

--- server/server.js
+++ server/server.js
@@ -3,32 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 const db = require('./config/db');
-//das das
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
   })
-
-app.get('/', (req, res) => {
-    res.send('Sejun Node Express Server 구동 완료');
-})
-
-app.get('/test', (req, res) => {
-    res.send('테스트 화면입니다.');
-})
-
-app.get('/data', (req, res) => {
-    res.send({ result : '서버 통신 완료' });
-})
-
-app.get('/get/db_data', (req, res) => {
-    db.query("select * from test", (err, data) => {
-        if(!err) {
-            res.send(data);
-
-        } else {
-            console.log(err);
-            res.send(err);
-        }
-    })
-})

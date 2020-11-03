1c04e3355f05374f69c235df4cf05a7411a709a3
Kim Sejun <sejun3278@gmail.com>
2020-11-03 16:39:39 +0900

    Updates

---
+++ precache-manifest.e073bd71e06c3a96e9e3cd2e06b129dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094260ced12ebabde2c472456a8f7813",
+    "url": "/sejun-redux-mall/index.html"
+  },
+  {
+    "revision": "5aafd84ffa8dd6d7e285",
+    "url": "/sejun-redux-mall/static/css/main.880b288b.chunk.css"
+  },
+  {
+    "revision": "cdc85d7849904d440349",
+    "url": "/sejun-redux-mall/static/js/2.5b5ac8d8.chunk.js"
+  },
+  {
+    "revision": "7b11fc738d2d93490d0bf157a2d0485b",
+    "url": "/sejun-redux-mall/static/js/2.5b5ac8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aafd84ffa8dd6d7e285",
+    "url": "/sejun-redux-mall/static/js/main.a6ffd251.chunk.js"
+  },
+  {
+    "revision": "aee82be7eaff67658e70",
+    "url": "/sejun-redux-mall/static/js/runtime-main.02671ea1.js"
+  }
+]);

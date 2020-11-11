d178743ac707aa160c8632e9b7c6a67d26f83669
Kim Sejun <sejun3278@gmail.com>
2020-11-11 19:40:51 +0900

    '2020-11-11-modify-2'

--- src/page/body/admin/index.js
+++ src/page/body/admin/index.js
@@ -1,6 +1,5 @@
 export { default as AdminHome } from './home';
 export { default as PassAdmin } from './pass';
-export { default as AdminLogin } from './login';
 export { default as AdminCategory } from './category';
 export { default as AdminGoods } from './goods';
 export { default as AdminOrder } from './order';
